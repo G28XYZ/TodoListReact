4bf79a07d797fec1d191faf301b47642946223cf
Aleksandr Kucherenko <g28xyz@ya.ru>
2021-12-31 19:11:41 +0700

    lesson-13 refresh state in listener with arrow function

--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -7,14 +7,16 @@ class TodoListItem extends Component {
   };
 
   onLabelClick = () => {
-    this.setState({
-      done: true,
+    this.setState(({ done }) => {
+      return { done: !done };
     });
   };
 
   onMarkImportant = () => {
-    this.setState({
-      important: true,
+    this.setState(({ important }) => {
+      return {
+        important: !important,
+      };
     });
   };
 
