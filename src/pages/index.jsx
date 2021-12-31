2631a76212c58fe54efa95451ce87181a26b7bfd
Aleksandr Kucherenko <g28xyz@ya.ru>
2021-12-31 13:21:49 +0700

    lesson-5 spread array in props component

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,11 +6,17 @@ import TodoList from "../components/TodoList";
 import SearchPanel from "../components/SearchPanel";
 
 const App = () => {
+  const toDoData = [
+    { label: "Drink Coffee", important: false },
+    { label: "Make Awesome App", important: true },
+    { label: "Have a lunch", important: false },
+  ];
+
   return (
     <div>
       <AppHeader />
       <SearchPanel />
-      <TodoList />
+      <TodoList todos={toDoData} />
     </div>
   );
 };
