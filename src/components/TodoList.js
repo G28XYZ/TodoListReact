3c03073324df65d0f4bc040af64e62660664fcc5
Aleksandr Kucherenko <g28xyz@ya.ru>
2021-12-31 13:39:43 +0700

    lesson-6 add key-id for map and spread itemProps

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,10 +3,11 @@ import React from "react";
 import TodoListItem from "./TodoListItem";
 
 const TodoList = ({ todos }) => {
+  const {id, ...itemProps}
   const elements = todos.map((item) => {
     return (
-      <li>
-        <TodoListItem {...item} />
+      <li key={item.id}>
+        <TodoListItem { ...itemProps } />
       </li>
     );
   });
