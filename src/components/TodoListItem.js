9f27eaddb2c649c0f5c1103ff050877674d3ba9e
Aleksandr Kucherenko <g28xyz@ya.ru>
2021-12-31 18:50:23 +0700

    lessont-12 add listener on important button and style important

--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -3,6 +3,7 @@ import React, { Component } from "react";
 class TodoListItem extends Component {
   state = {
     done: false,
+    important: false,
   };
 
   onLabelClick = () => {
@@ -11,27 +12,35 @@ class TodoListItem extends Component {
     });
   };
 
+  onMarkImportant = () => {
+    this.setState({
+      important: true,
+    });
+  };
+
   render() {
-    const { label, important = false } = this.props;
-    const { done } = this.state;
+    const { label } = this.props;
+    const { done, important } = this.state;
 
     let classNames = "todo-list-item";
     if (done) {
       classNames += " done";
     }
-
-    const style = {
-      color: important ? "steelblue" : "black",
-      fontWeight: important ? "bold" : "normal",
-    };
+    if (important) {
+      classNames += " important";
+    }
 
     return (
       <span className={classNames}>
-        <span className="todo-list-item-label" style={style} onClick={this.onLabelClick}>
+        <span className="todo-list-item-label" onClick={this.onLabelClick}>
           {label}
         </span>
 
-        <button type="button" className="btn btn-outline-success btn-sm float-right">
+        <button
+          type="button"
+          className="btn btn-outline-success btn-sm float-right"
+          onClick={this.onMarkImportant}
+        >
           <i className="fa fa-exclamation" />
         </button>
 
