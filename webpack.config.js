f40e7b704432595103d80e272cd4d8f37e0180c1
Aleksandr Kucherenko <g28xyz@ya.ru>
2021-12-31 12:20:44 +0700

    lesson-1 create elements

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  entry: { main: "./src/pages/index.js" },
+  entry: { main: "./src/pages/index.jsx" },
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "main.js",
@@ -20,7 +20,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.(js|jsx)$/,
         use: "babel-loader",
         exclude: "/node_modules/",
       },
