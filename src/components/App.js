27a3c40f66bc8c05e8f71bf84e5a1456935f8f7d
Aleksandr Kucherenko <g28xyz@ya.ru>
2021-12-31 20:13:25 +0700

    lesson-15 complite function deleteItem in App

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,39 @@
-import React from "react";
+import React, { Component } from "react";
 
 import AppHeader from "../components/AppHeader";
 import TodoList from "../components/TodoList";
 import SearchPanel from "../components/SearchPanel";
 import ItemStatusFilter from "../components/ItemStatusFilter";
 
-const App = () => {
-  const todoData = [
-    { label: "Drink Coffee", important: false, id: 1 },
-    { label: "Make Awesome App", important: true, id: 2 },
-    { label: "Have a lunch", important: false, id: 3 },
-  ];
+class App extends Component {
+  state = {
+    todoData: [
+      { label: "Drink Coffee", important: false, id: 1 },
+      { label: "Make Awesome App", important: true, id: 2 },
+      { label: "Have a lunch", important: false, id: 3 },
+    ],
+  };
 
-  return (
-    <div className="todo-app">
-      <AppHeader toDo={1} done={3} />
-      <div className="top-panel d-flex">
-        <SearchPanel />
-        <ItemStatusFilter />
+  deleteItem = (id) => {
+    this.setState(({ todoData }) => {
+      const idx = todoData.findIndex((element) => element.id === id);
+      const newArray = [...todoData.slice(0, idx), ...todoData.slice(idx + 1)];
+      return { todoData: newArray };
+    });
+  };
+
+  render() {
+    return (
+      <div className="todo-app">
+        <AppHeader toDo={1} done={3} />
+        <div className="top-panel d-flex">
+          <SearchPanel />
+          <ItemStatusFilter />
+        </div>
+        <TodoList todos={this.state.todoData} onDeleted={this.deleteItem} />
       </div>
-      <TodoList todos={todoData} onDeleted={(id) => console.log(id)} />
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default App;
